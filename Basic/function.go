4b5a6f9be8e739355e913ff5b5264a5548bfaf21
unknown <aidenpearcewd01@gmail.com>
2022-05-13 16:39:13 +0700

    'update'

--- Basic/function.go
+++ Basic/function.go
@@ -4,19 +4,32 @@ import (
 	"fmt"
 )
 
-func average (num int, nums ...int) {
+
+// Normal Function
+func sum (nums []int) int {
+	sum:=0
+	for _,v := range (nums){
+		sum +=v
+	}
+
+	return sum
+}
+
+// Variadic Function
+func variadic_average (nums ...int) int {
 	avg := 0
-	for i,v:= range (nums) {
-		fmt.Println(i)
+	for _,v:= range (nums) {
 		avg += v
 	}
 
-	fmt.Println(avg)
 	return (avg/len(nums))
 }
 
 func main() {
 	_list:= []int{1,2,3,4,5,6,7,8,9}
-	_avg := average(_list)
-	fmt.Println(_avg)
+	_total:= sum(_list)
+	_avg := variadic_average(_list...)
+
+	fmt.Println("Total:",_total)
+	fmt.Println("Average:",_avg)
 }
